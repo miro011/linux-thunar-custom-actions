6c6c9f7bd6f4910f5b395b968bb209880d69de96
miro011 <31739501+miro011@users.noreply.github.com>
2022-11-30 23:37:19 -0700

    wrong scirpt
    was a duplicate of backup.py
    now it's the actual script

--- create-shortcut.py
+++ create-shortcut.py
@@ -1,55 +1,37 @@
+# Creates shortcuts to the selected file/s
+# Syntact python3 "script name" "filepath1" "filepath2"...
+
 # sys.argv[0] is the name of the script
-# sys.argv[1] is the source folder
+# sys.argv[1:] the filepaths
+
+#########################################################
 
 import os, sys
 
-SOURCE_PATH = ""
-DEST_PATH = ""
+#########################################################
+ERRORS = ""
 
 def run():
-    init()
-    dest_path_user_input()
-    run_rsync_backup()
-    
-def init():
-    global SOURCE_PATH
-    if len(sys.argv) != 2: input("Invalid number of arguments. Try again."); quit()
-    if not os.path.isdir(sys.argv[1]): input("Invalid source directory argument. Try again."); quit()
-    SOURCE_PATH = sys.argv[1]
-    if SOURCE_PATH.endswith("/"): SOURCE_PATH = SOURCE_PATH[:-1]
-    
-    print("*** WARNING, this will clone the source folder to the destination folder !!!\n")
-    print(f"*** SOURCE FOLDER:\n{SOURCE_PATH}\n")
-    input("Press ENTER to continue")
-
-def dest_path_user_input():
-    global DEST_PATH
-    
-    os.system("clear")
+    global ERRORS
+    for filePath in sys.argv[1:]:
+        if not os.path.exists(filePath): ERRORS += f"Invalid path:\n{filePath}\n\n"; continue
+        parentFolder = os.path.dirname(filePath)
+        if not os.access(parentFolder, os.W_OK): ERRORS += f"No write permission to:\n{parentFolder}\n\n"; break
+        shortcutPath = get_unused_path(f"{filePath} - Shortcut")
+        os.system(f"ln -s '{sq(filePath)}' '{sq(shortcutPath)}' > /dev/null 2>&1")
+    if ERRORS:
+        os.system(f"mate-terminal --name 'thunar' --title 'CREATE SHORTCUT ERRORS' -- sh -c 'printf \"{sq(ERRORS)}\"; read -p \"PRESS ENTER TO CLOSE\" hold;' > /dev/null 2>&1")
+        
+# given the path of a file/folder, it returns one that doesn't exist (numbered if original one exists)
+def get_unused_path(filePath):
+    if not os.path.exists(filePath): return filePath
+    counter = 1
     while 1==1:
-        usrInput = input("ENTER DESTINATION PATH:\n").strip()
-        os.system("clear")
-        
-        if usrInput.endswith("/"): usrInput = usrInput[:-1]
-        
-        if not os.path.isdir(usrInput): print("*** Invalid directory, try again"); continue
-        elif SOURCE_PATH in usrInput: print("*** Destination can't be a sub-directory of source, try again."); continue
-        elif usrInput in SOURCE_PATH: print("*** Destination can't be a parent directory of source, try again."); continue
-        
-        while 1==1:
-            usrInputConfirm = input(f"DESTINATION YOU ENTERED:\n{usrInput}\n\nARE YOU SURE? (y/n)\n").strip().lower()
-            os.system("clear")
-            if usrInputConfirm == "y": DEST_PATH = usrInput; return
-            elif usrInputConfirm == "n": break
-            else: print("*** Invalid response, try again"); continue
+        newFilePath = f"{filePath} ({counter})"
+        if not os.path.exists(newFilePath): return newFilePath
+        counter += 1
 
-def run_rsync_backup():
-    os.system("clear")
-    print(f"SOURCE:\n{SOURCE_PATH}\n\nDESTINATION:\n{DEST_PATH}\n")
-    print("----------------------------\nPERFORMING BACKUP\n----------------------------\n\n")
-    # rsync -av --delete "/src/dir/" "/dest/dir/"
-    os.system(f"rsync -av --delete \"{SOURCE_PATH}/\" \"{DEST_PATH}/\"")
-    print("\n\n----------------------------\nOPERATION COMPLETE\n----------------------------\n")
-    input("PRESS ENTER TO EXIT")
+def sq(str):
+    return str.replace("'", "'\"'\"'")
 
 run()
